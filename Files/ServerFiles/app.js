95541c4a424e1de5c094d91f6a660975427be358
Stephen Chan <crashingturtle22@gmail.com>
2018-12-05 09:43:40 -0500

    Added header titles & Favicon
    Whatever the names are when you open it in chrome (the top tab)
    
    Though the fav is just a circle rn

--- Files/ServerFiles/app.js
+++ Files/ServerFiles/app.js
@@ -1,30 +1,35 @@
 let express = require('express');
 let path = require('path');
+
 const PORT = process.env.PORT || 3000
 
 var app = express();
 
+
 app.use(function (req, res, next) {
+
+    console.log(req.originalUrl);
+
+
 
     if(req.originalUrl.endsWith('.html')){
 //        console.log(path.join(__dirname,'/../WebFiles/HTML'+req.originalUrl))
-        res.sendFile(path.join(__dirname,'/../WebFiles/HTML'+req.originalUrl));
+        res.sendFile(path.join(__dirname,'..','WebFiles','HTML',req.originalUrl));
     }
     else if (req.originalUrl === '/') {
-        res.sendFile(path.join(__dirname,'/../WebFiles/HTML/main.html'));
+        res.sendFile(path.join(__dirname,'..','WebFiles','HTML','main.html'));
     }
     else {
-        res.sendFile(path.join(__dirname ,'/../WebFiles' + req.originalUrl));
+        res.sendFile(path.join(__dirname,'..','WebFiles',req.originalUrl));
     }
     
 });
+
+
 /*
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '/../WebFiles/HTML/main.html'))
 })
-
-app.get('*.html', function (req, res) {
-    res.sendFile(path.join(__dirname + '/../WebFiles/HTML/about.html'))
-})*/
+*/
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
